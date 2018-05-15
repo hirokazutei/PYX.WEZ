33b1f653845b84657084d633a485361ea5f6fa08
Josh Teneycke <josh.teneycke@gmail.com>
2018-05-15 03:48:57 -0400

    Found the seam, time to start unravelling to re-spool the thread!

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -15,28 +15,34 @@ import "./InfoPage.js"
 import "./Custom.js"
 
 export default function sketch(s) {
-  let x, y, backgroundColor;
+  //s.setup = () => {
 
-  const width = 500;
-  const height = 500;
 
-  s.setup = () => {
-    s.createCanvas(width, height);
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
-
-    x = s.random(width);
-    y = height / 2;
-  };
-
-  s.draw = () => {
-    s.background(backgroundColor);
-    s.fill(s.color(255, 0, 0));
-    s.ellipse(x, y, 100, 100);
-
-    x = (x + 1) % width;
-  };
-
-  s.mousePressed = () => {
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
-  };
 }
+
+//export default function sketch(s) {
+  //let x, y, backgroundColor;
+
+  //const width = 500;
+  //const height = 500;
+
+  //s.setup = () => {
+    //s.createCanvas(width, height);
+    //backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
+
+    //x = s.random(width);
+    //y = height / 2;
+  //};
+
+  //s.draw = () => {
+    //s.background(backgroundColor);
+    //s.fill(s.color(255, 0, 0));
+    //s.ellipse(x, y, 100, 100);
+
+    //x = (x + 1) % width;
+  //};
+
+  //s.mousePressed = () => {
+    //backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
+  //};
+//}
