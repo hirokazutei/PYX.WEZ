c60b98480826cdb7ee86109e6e9d773b59bcad6a
Josh Teneycke <josh.teneycke@gmail.com>
2018-05-15 02:43:37 -0400

    Move images into assets

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import 'p5/lib/addons/p5.dom';
 
 import sketch from './sketch';
 
+    //"Sketch.js"
+    //"PixelObjects.js"
+    //"Text.js"
+    //"Cities.js"
+    //"Circle.js"
+    //"World.js"
+    //"APIFunctions.js"
+    //"Pixel.js"
+    //"Time.js"
+    //"Sky.js"
+    //"SunMoon.js"
+    //"Weather.js"
+    //"WeatherSeverity.js"
+    //"InfoPage.js"
+    //"Custom.js"
+
+
+
 new p5(sketch);
 
